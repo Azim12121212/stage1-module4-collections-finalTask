65d078ed5db13aa0507da1b153591bbb2afc282d
Azimbek <omar.azimbek@gmail.com>
2024-01-22 12:30:18 +0600

    1.4.finalTask Commit

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,22 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> projectNames = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> e: projects.entrySet()) {
+            if (e.getValue().contains(developer)) {
+                projectNames.add(e.getKey());
+            }
+        }
+
+        if (!projectNames.isEmpty()) {
+            projectNames.sort(Comparator.reverseOrder());
+            projectNames.sort(Comparator.comparing(String::length).reversed());
+        }
+
+        return projectNames;
     }
 }
